658cfe378c0eceb047987480b78b9564a80197cc
Clemente Acosta <clems.acosta@gmail.com>
2022-07-21 23:38:30 -0500

    Added social media section with button toggle

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('namae')
+const $ = selector => document.querySelector(selector)
+const socialDiv = $('#social-media')
+
+document.querySelectorAll('#btn-share').forEach(element => {
+    element.addEventListener('click', event => {
+      socialDiv.classList.toggle("hidden")
+    })
+  }
+)
